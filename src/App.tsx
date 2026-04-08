203d384fd36a73af5f72ca9d9f32a570bf571660
cyrian01 <cyrian206@gmail.com>
2026-04-08 12:32:49 +0300

    lastChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from "./components/Container"
+import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React AppFront</h1>
-      <h2>Let's learn React JS 2026</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Header />
+        <h1>On va apprendre ensemble, Javascript ECM6 et Reactjs</h1>
+      </Container>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <span className="content-header">Mon Header</span>
+    </header>
+  );
+};
+
+export default Header;
